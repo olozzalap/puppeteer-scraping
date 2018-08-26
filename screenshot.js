9c7ceb252f3d698a1f8d3986ed7a5fbd676f5ea2
Eben Palazzolo <olozzalap@gmail.com>
2018-08-26 15:49:17 -0400

    Added optional fileName argument for calling node screenshot.js to define the filename

--- screenshot.js
+++ screenshot.js
@@ -4,12 +4,13 @@ const url = process.argv[2];
 if (!url) {
     throw "Please provide a URL as the first argument";
 }
+const fileName = process.argv[3] || 'screenshot';
 
 async function run () {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(url);
-    await page.screenshot({path: 'screenshot.jpg'});
+    await page.screenshot({path: fileName + '.jpg'});
     browser.close();
 }
 run();
